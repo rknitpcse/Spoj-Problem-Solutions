0f545bd4a2fa9a800b814b87aace11cd52cf3f0a
rknitpcse <47469003+rknitpcse@users.noreply.github.com>
2019-07-14 16:26:59 +0530

    Add files via upload

---
+++ PRLGAME2.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+**RAKESH KUMAR** 
+*/
+int main() {
+	int cs = 1, n, p, q;
+	while(scanf("%d %d %d", &n, &p, &q) == 3) printf("Case %d: %.4lf\n", cs++, (double)(q+1) / (p+2));
+	return 0;
+}
+
